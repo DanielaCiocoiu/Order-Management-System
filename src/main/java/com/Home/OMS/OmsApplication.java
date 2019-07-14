94f3e34ea4b914e4b54173d855a7f589ee83ad1e
shantiox <danaciocoiu78@gmail.com>
2019-07-14 16:29:59 +0300

    Controller, entity, repository, Exception, Product,

--- src/main/java/com/Home/OMS/OmsApplication.java
+++ src/main/java/com/Home/OMS/OmsApplication.java
@@ -1,9 +1,18 @@
 package com.Home.OMS;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+ 
+@ServletComponentScan
+@SpringBootApplication(scanBasePackages = "com.Home.OMS")
+@EnableJpaRepositories("com.Home.OMS.repository")
+@EntityScan("com.Home.OMS.entity")
 public class OmsApplication {
 
 	public static void main(String[] args) {
